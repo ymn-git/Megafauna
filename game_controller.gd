94ce22a8a24f2f50c05e461d5deab555bbf236b3
ymn-git <yairmn@hotmail.com>
2024-10-01 17:31:58 -0300

    Primer Commit

---
+++ game_controller.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var playerLife = 3
+
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func lost_life():
+	playerLife-=1
+	if playerLife==0:
+		var hud = load("res://juego/Scenes/fondo/hud.tscn").instantiate()
+		get_tree().root.add_child(hud)
+		get_tree().current_scene = hud
+		
+		
---
+++ scripts/miramar.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+
+func _on_portal_body_entered(body):
+	
+	var level2 = load("res://juego/Scenes/Niveles/miramar3.tscn").instantiate()
+	get_tree().root.add_child(level2)
+	get_tree().current_scene = level2
+	queue_free()
